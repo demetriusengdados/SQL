6f623a37f85e12b6056b53ad99f61da246d1b475
demetriusengdados <dimagela25@gmail.com>
2023-07-31 11:11:58 -0300

    first commit

---
+++ select.sql
@@ -0,0 +1,20 @@
+select * from costumer;
+select customer_id, name from customer;
+select * from product 
+
+select
+  product_id,
+  description,
+  price,
+ where price * 1.07 as taxed_price
+ from product;
+
+select
+  product_id,
+  description,
+  price as untaxed_price,
+  from costumer
+ where price 1.07 as taxed_price
+ from product
+
+ 
